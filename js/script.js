bab931cf33c0cae6cfedb84d90207d6482d04b07
Полина <polina.uladykouskaya@gmail.com>
2026-03-14 20:26:31 +0300

    feat: implement responsive layout according to Figma template

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.getElementById('myButton');
+  const message = document.getElementById('message');
 
-    const button = document.getElementById('myButton');
-    const message = document.getElementById('message');
-
-    button.addEventListener('click', function() {
-        message.textContent = 'кнопка нажата';
-        message.style.color = '#4CAF50';
+  if (button) {
+    button.addEventListener('click', function () {
+      message.textContent = 'кнопка нажата';
+      message.style.color = '#4CAF50';
     });
+  }
 
-       console.log('cтраница загружена и готова к работе');
+  console.log('страница загружена и готова к работе');
+
+  const burger = document.getElementById('burger');
+  const nav = document.getElementById('nav');
+
+  if (burger) {
+    burger.addEventListener('click', function () {
+      nav.classList.toggle('active');
     });
+  }
+});
